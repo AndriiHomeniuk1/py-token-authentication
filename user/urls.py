446849abc21da385bed53017b41b9e79c5c8a441
Andrii Homeniuk <andreygomenuk@gmail.com>
2026-06-25 12:46:43 +0300

    feat(user): add CreateUserView and register endpoint
    - Implemented CreateUserView using UserSerializer
    - Added "register/" path in user/urls.py with name="create"

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
+from user.views import CreateUserView
+
 
 app_name = "user"
 
-urlpatterns = []
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+]
